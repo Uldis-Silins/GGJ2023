ce4c6296f7189b5a8d1437162b5780ceb8de9b75
Uldis Silins <usil12965@gmail.com>
2023-02-05 06:21:14 +0200

    audio detect temp

--- Assets/Scripts/AudioDetection/AudioDetection.cs
+++ Assets/Scripts/AudioDetection/AudioDetection.cs
@@ -5,6 +5,33 @@ using UnityEngine;
 public class AudioDetection : MonoBehaviour
 {
     public int sampleWindow = 64;
+    public AudioSource micSource;
+
+    private AudioClip m_microphoneClip;
+
+    private float[] m_samples = new float[512];
+
+    private void Start()
+    {
+        GetMicrophoneAudio();
+    }
+
+    public void GetMicrophoneAudio()
+    {
+        string microphoneName = Microphone.devices[0];
+        m_microphoneClip = Microphone.Start(microphoneName, true, 20, AudioSettings.outputSampleRate);
+        micSource.clip = m_microphoneClip;
+    }
+
+    public float GetLoudnessFromMicrophone()
+    {
+        return GetLoudness(m_microphoneClip, Microphone.GetPosition(Microphone.devices[0]));
+    }
+
+    public void GetSpectrum()
+    {
+        micSource.GetSpectrumData(m_samples, 0, FFTWindow.Blackman)
+    }
 
     public float GetLoudness(AudioClip clip, int position)
     {
@@ -19,6 +46,6 @@ public class AudioDetection : MonoBehaviour
             totalLoudness += Mathf.Abs(sampleData[i]);
         }
 
-        return totalLoudness;
+        return totalLoudness / sampleWindow;
     }
 }
